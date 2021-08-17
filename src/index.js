d771e462a413a4cdc5a3e0fa4b98e936bb235318
ConstantineTU <8kozmo8@gmail.com>
2021-08-17 21:26:05 +0500

    init: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+
+
+
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0
+  }
+  let minValue = array[0]
+  for (let i = 0; i < array.length; i++) {
+    if (minValue > array[i]) {
+      minValue = array[i]
+    }
+  }
+
+  return minValue
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0
+  }
+  let maxValue = array[0]
+  for (let i = 0; i < array.length; i++) {
+    if (maxValue < array[i]) {
+      maxValue = array[i]
+    }
+  }
+
+  return maxValue
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0
+  }
+  let avgValue = 0
+  for (let i = 0; i < array.length; i++) {
+    avgValue += array[i]
+  }
+  avgValue = avgValue / array.length
+
+  return avgValue
 }
